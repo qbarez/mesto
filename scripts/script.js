0b319575b6249761a4efa91eb4a931c4e16ed81a
Valeriy Gogin <qbarez@yandex.ru>
2022-03-27 14:27:15 +0300

    feat: Добавить фукцию редактирования полей формы профиля

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,24 @@ popup.addEventListener('click', function(event){
 editProfile.addEventListener('click', togglePopup);
 closeProfile.addEventListener('click', togglePopup);
 
+let profileName = document.querySelector('.profile__title');
+let profileInfo = document.querySelector('.profile__subtitle');
+let popupContainer = document.querySelector('.popup__container');
+let formElement = popupContainer.querySelector('.popup__form');
+let inputName = formElement.querySelector('input[name="name"]');
+let inputInfo = formElement.querySelector('input[name="info"]');
+
+inputName.value = profileName.innerHTML;
+inputInfo.value = profileInfo.innerHTML;
+
+function formSubmitHandler (evt) {
+	evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileInfo.textContent = inputInfo.value;
+  togglePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
 
 
-console.log('ya tut');
 
