f271744c7305b259ef03dc96efb35054b89935ff
Valeriy Gogin <qbarez@yandex.ru>
2022-05-16 12:29:33 +0300

    fix: Правки по замечаниям ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 let popup = document.querySelector('.popup');
+
 let editProfile = document.querySelector('.profile__edit-button');
+
 let closeProfile = document.querySelector('.popup__close-button');
+
+let profileName = document.querySelector('.profile__title');
+
+let profileInfo = document.querySelector('.profile__subtitle');
+
+let popupContainer = document.querySelector('.popup__container');
+
+let formElement = popupContainer.querySelector('.popup__form');
+
+let inputName = formElement.querySelector('input[name="name"]');
+
+let inputInfo = formElement.querySelector('input[name="info"]');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 };
 
-popup.addEventListener('click', function(event){
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-});
-
-editProfile.addEventListener('click', togglePopup);
-closeProfile.addEventListener('click', togglePopup);
-
-let profileName = document.querySelector('.profile__title');
-let profileInfo = document.querySelector('.profile__subtitle');
-let popupContainer = document.querySelector('.popup__container');
-let formElement = popupContainer.querySelector('.popup__form');
-let inputName = formElement.querySelector('input[name="name"]');
-let inputInfo = formElement.querySelector('input[name="info"]');
-
-inputName.value = profileName.innerHTML;
-inputInfo.value = profileInfo.innerHTML;
+function saveProfileData() {
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileInfo.textContent;
+};
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
@@ -32,7 +32,16 @@ function formSubmitHandler (evt) {
   togglePopup();
 }
 
+editProfile.addEventListener('click', togglePopup);
+
+editProfile.addEventListener('click', saveProfileData);
+
+closeProfile.addEventListener('click', togglePopup);
+
 formElement.addEventListener('submit', formSubmitHandler);
 
-
-
+/*popup.addEventListener('click', function(event){
+  if (event.target === event.currentTarget) {
+    togglePopup();
+  }
+});*/
