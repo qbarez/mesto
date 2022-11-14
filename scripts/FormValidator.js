eedb3e78ca64615dbd18946c87e64ebe082c2b13
Valeriy Gogin <qbarez@yandex.ru>
2022-11-14 20:24:18 +0300

    7prj refactoring validation

---
+++ scripts/FormValidator.js
@@ -0,0 +1,95 @@
+export class FormValidator {
+
+  constructor(formElement, settings) {
+    this._form = formElement;
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._inactiveButton = settings.inactiveButtonClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._errorClass = settings.errorClass;
+    this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
+    this._bottonElement = this._form.querySelector(this._submitButtonSelector)
+  
+    /*export const validationSettings = {
+        formSelector: '.popup__form',
+        inputSelector: '.popup__form-input',
+        submitButtonSelector: '.popup__submit-button',
+        inactiveButtonClass: 'popup__submit-button_inactive',
+        inputErrorClass: 'popup__form-input_invalid',
+        errorClass: '.popup__form-input-error',
+      };*/
+    }
+ 
+    _setEventListeners() {
+      
+      this._toggleBtnState();
+
+      this._inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', () => {
+          this._errorElement = this._form.querySelector(`#${inputElement.id}-error`);
+          this._checkInputValidity(inputElement);
+          this._toggleBtnState();
+          console.log("setEventListeners работает");
+        });
+      });
+    }
+
+    removeInputErrors () {
+        this._toggleBtnState(this._bottonElement);
+      
+        this._inputList.forEach((input) => {
+        this._errorElement = this._form.querySelector(`.${input.id}-error`);
+        this._hideInputError(input);
+        })
+        console.log("removeInputErrors работает");
+      }
+
+    _checkInputValidity (inputElement) {
+        if (!inputElement.validity.valid) {
+          this._showInputError(inputElement);
+          console.log("checkInputValidity работает");
+        } else {
+          this._hideInputError(inputElement);
+          console.log("else checkInputValidity работает");
+        }
+    };
+
+    _showInputError (inputElement) {
+        inputElement.classList.add(this._inputErrorClass);
+        this._errorElement.textContent = inputElement.validationMessage;
+        this._errorElement.classList.add(this._errorClass);
+        console.log("showInputError работает");
+      };  
+
+    _hideInputError (inputElement) {
+      if (!this._errorElement) return;
+      inputElement.classList.remove(this._inputErrorClass);
+      this._errorElement.classList.remove(this._errorClass);
+      this._errorElement.textContent = '';
+      console.log("hideInputError работает");
+    };
+
+    _validateInput() {
+      return this._inputList.some((input) => !input.validity.valid);
+    }
+
+    _toggleBtnState() {
+        if (this._validateInput()) {
+            this._bottonElement.classList.add(this._inactiveButton);
+            this._bottonElement.setAttribute('disabled', true);
+            console.log("toggleBtnState работает");
+        } else {
+            this._bottonElement.classList.remove(this._inactiveButton);
+            this._bottonElement.removeAttribute('disabled', true);
+            console.log("toggleBtnState else работает");
+          }
+      } 
+
+    enableValidation() {
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        this._setEventListeners();
+        console.log("enableValidaton работает");
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import { FormValidator } from "./FormValidator.js";
+
 const profilePopup = document.querySelector('#popup_edit_profile');
 const profileEditbutton = document.querySelector('.profile__edit-button');
 const profileCloseButton = document.querySelector('#profile_close_botton');
@@ -49,6 +51,15 @@ const initialCards = [
   }
 ];
 
+export const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__form-input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_inactive',
+  inputErrorClass: 'popup__form-input_invalid',
+  errorClass: '.popup__form-input-error',
+};
+
 const openPopup = (element) => {
   element.classList.add('popup_opened');
   document.addEventListener('keyup', closePopupEscKey);
@@ -68,7 +79,6 @@ const submitProfileForm = (event) => {
 	event.preventDefault();
   profileName.textContent = profileNameInput.value;
   profileInfo.textContent = profileInfoInput.value;
-  setInactiveButtonClass(profileFormSubmitButton, validationSettings);
   closePopup(profilePopup);
 }
 
@@ -76,7 +86,6 @@ const addNewCard = (event) => {
   event.preventDefault();
   cardParam = addInitCards(newPlaceName.value, newPlaceImage.value);
   renderCard(cardParam, cardsContainer);
-  setInactiveButtonClass(newPlaceSubmitButton, validationSettings);
   closePopup(newPlacePopup);
 };
 
@@ -132,8 +141,7 @@ const closePopupOverlayClick = (event) => {
 profileEditbutton.addEventListener('click', () => {
   openPopup(profilePopup);
   renderProfileInputs();
-  removeInputErrors(profilePopup);
-  setActiveButtonClass(profileFormSubmitButton, validationSettings);
+  profileFormValidation.removeInputErrors();
 });
 
 profileCloseButton.addEventListener('click', () => {
@@ -149,7 +157,7 @@ profileForm.addEventListener('submit', submitProfileForm);
 placeAddButton.addEventListener('click',() => {
   openPopup(newPlacePopup);
   newPlaceForm.reset();
-  removeInputErrors(newPlacePopup);
+  newPlaceFormValidation.removeInputErrors(newPlacePopup);
 });
 
 newPlaceForm.addEventListener('submit', addNewCard);
@@ -161,3 +169,105 @@ imagePopupCloseButton.addEventListener('click', () => {
 newPlacePopup.addEventListener('click', closePopupOverlayClick);
 profilePopup.addEventListener('click', closePopupOverlayClick);
 imagePopup.addEventListener('click', closePopupOverlayClick);
+
+const profileFormValidation = new FormValidator(profileForm, validationSettings);
+profileFormValidation.enableValidation();
+const newPlaceFormValidation = new FormValidator(newPlaceForm, validationSettings);
+newPlaceFormValidation.enableValidation();
+
+/*
+const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__form-input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_inactive',
+  inputErrorClass: 'popup__form-input_invalid',
+  errorClass: '.popup__form-input-error',
+};
+
+const showInputError = (formElement, inputElement, errorMessage, validationSettings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(validationSettings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(validationSettings.errorClass);
+  //console.log("showInputError работает");
+};
+
+const hideInputError = (formElement, inputElement, validationSettings) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(validationSettings.inputErrorClass);
+  errorElement.classList.remove(validationSettings.errorClass);
+  errorElement.textContent = '';
+  //console.log("hideInputError работает");
+};
+
+const removeInputErrors = (popup) => {
+  const inputList = Array.from(popup.querySelectorAll(validationSettings.inputSelector));
+
+  inputList.forEach((inputElement) => {
+      hideInputError(popup, inputElement, validationSettings);
+  })
+  //console.log("removeInputErrors работает");
+}
+
+const setInactiveButtonClass = (buttonElement, validationSettings) => {
+  buttonElement.classList.add(validationSettings.inactiveButtonClass);
+  buttonElement.setAttribute('disabled', true);
+  //console.log("setInactiveButtonClass работает");
+}
+
+const setActiveButtonClass = (buttonElement, validationSettings) => {
+  buttonElement.classList.remove(validationSettings.inactiveButtonClass);
+  buttonElement.removeAttribute('disabled', true);
+  //console.log("setActiveButtonClass работает");
+}
+
+const enableValidation = (validationSettings) => {
+  const formList = Array.from(document.querySelectorAll(validationSettings.formSelector));
+  formList.forEach((formElement) => {
+      formElement.addEventListener('submit', (evt) => {
+          evt.preventDefault();
+      });
+
+      setEventListeners(formElement, validationSettings);
+  });
+  //console.log("enableValidation работает");
+};
+
+const checkInputValidity = (formElement, inputElement, validationSettings) => {
+  if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage, validationSettings);
+      //console.log("checkInputValidity работает");
+  } else {
+      hideInputError(formElement, inputElement, validationSettings);
+      //console.log("else checkInputValidity работает");
+  }
+};
+
+const setEventListeners = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
+  const buttonElement = formElement.querySelector(validationSettings.submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+          checkInputValidity(formElement, inputElement, validationSettings);
+          toggleBtnState(buttonElement, inputList, validationSettings);
+          //console.log("setEventListeners работает");
+      });
+  });
+  //console.log("setEventListeners работает");
+};
+
+const validateInput = (inputList) => inputList.some((inputElement) => !inputElement.validity.valid);
+
+const toggleBtnState = (buttonElement, inputList, validationSettings) => {
+  if (validateInput(inputList)) {
+      setInactiveButtonClass(buttonElement, validationSettings);
+      //console.log("toggleBtnState работает");
+  } else {
+      setActiveButtonClass(buttonElement, validationSettings);
+      //console.log("toggleBtnState else работает");
+  }
+};
+
+enableValidation(validationSettings);*/
